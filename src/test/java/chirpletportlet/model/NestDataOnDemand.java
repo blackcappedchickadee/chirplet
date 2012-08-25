01cce8d235974e5e07b5c44d9e3d66e3e8759780
Cloudchirp <cloudchirp.chirpmaster@gmail.com>
2012-08-25 16:59:42 -0400

    entity jpa --class ~.model.Nest --testAutomatically

---
+++ src/test/java/chirpletportlet/model/NestDataOnDemand.java
@@ -0,0 +1,7 @@
+package chirpletportlet.model;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Nest.class)
+public class NestDataOnDemand {
+}
---
+++ src/test/java/chirpletportlet/model/NestIntegrationTest.java
@@ -0,0 +1,12 @@
+package chirpletportlet.model;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Nest.class)
+public class NestIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
